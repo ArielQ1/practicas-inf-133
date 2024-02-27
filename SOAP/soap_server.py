160c9817f3986e0909bba83a888ef3b68c32b44c
ArielQ1 <quizayaariel@gmail.com>
2024-02-27 16:29:59 -0400

    endpoint NumberToDollars y

---
+++ SOAP/soap_server.py
@@ -0,0 +1,27 @@
+from http.server import HTTPServer
+from pysimplesoap.server import SoapDispatcher, SOAPHandler
+
+
+def sumaNumeros(num1, num2):
+    return "La suma de los numeros es : {}".format(num1+num2)
+
+dispatcher = SoapDispatcher(
+    "suma-de-numeros",
+    location="http://localhost:8000/",
+    action="http://localhost:8000/",
+    namespace="http://localhost:8000/",
+    trace = True,
+    ns = True,
+)
+
+dispatcher.register_function(
+    "SumaDosNumeros",
+    sumaNumeros,
+    returns={"resultado": int},
+    args={"num1": int, "num2": int}
+)
+
+server = HTTPServer(("0.0.0.0", 8000),SOAPHandler)
+server.dispatcher = dispatcher
+print("Servidor SOAP iniciado en http://localhost:8000/")
+server.serve_forever()
